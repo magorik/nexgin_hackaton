44c982b09d35fe0e69c8a6d7859f4c4f37f769b6
Nikolaevb <bogdan.nikolaev@geropharm.com>
2018-12-08 19:15:06 +0300

    Генератор с json

--- algoritms/generate.py
+++ algoritms/generate.py
@@ -8,16 +8,38 @@ Created on Sat Dec  8 15:21:22 2018
 import user_struct
 import random as rnd
 from datetime import datetime
-
-print (datetime.now())
-
-user_dic = {}
-
-for key in range(100000):
-    user_dic[key] = user_struct.user(key, rnd.uniform(0, 10000), rnd.uniform(0, 10000), datetime.utcnow())
+import numpy as np
+import time
 
 
-print (datetime.now())
+def generate_points(count, maximum_range_float):
+    time_a = datetime.now()
+    user_dic = {}
+    for line in range(count):
+        #result = user_struct.user(line, rnd.uniform(0, 10000), rnd.uniform(0, 10000), datetime.utcnow())
+        #print(result)
+        #result_array = np.append(result_array, [result], axis=count)
+        system_time = time.time()
+        user_dic[line] = user_struct.user(line, rnd.uniform(0, maximum_range_float), rnd.uniform(0, maximum_range_float), system_time)
+        #print(user_dic[line].to_json())
+    print (datetime.now() - time_a)
+    return user_dic
+
+def generate_timestap(user_dict, count, time_delay):
+    time_a = time.time()
+    for line in range(count):
+        user_dict[line]._x = user_dict[line]._x + rand_float(-1.0, 1.0)
+        user_dict[line]._y = user_dict[line]._y + rand_float(-1.0, 1.0)
+        user_dict[line]._timestap = time_a
+    return(user_dict)    
+    
+def rand_float(minimum, maximum):
+    return minimum + (maximum - minimum) * rnd.random()
+
+
+
+
+
     
 
 
--- algoritms/user_struct.py
+++ algoritms/user_struct.py
@@ -4,11 +4,15 @@ Created on Sat Dec  8 15:24:44 2018
 
 @author: plotb
 """
+import json
+#from collections import OrderedDict
+
 class user:
     def __init__(self, user_id, x, y, timestap):
         self._user_id = user_id
         self._x = x
         self._y = y
         self._timestap = timestap
-    
-    
+        
+    def to_json(self):
+        return json.dumps(self.__dict__)
